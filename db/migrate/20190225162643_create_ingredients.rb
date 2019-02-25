5a1da13b640d6916ce2a7069473c984675111ec7
Albert Campillo <albertcampillo@gmail.com>
2019-02-25 17:27:02 +0100

    Model Ingredient created

---
+++ db/migrate/20190225162643_create_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateIngredients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :ingredients do |t|
+      t.string :name
+      t.integer :price_100gr
+      t.integer :kcal_100gr
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
