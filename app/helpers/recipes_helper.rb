bbe2d5c6c56163c16394eca27ad7a9c3b5b2d539
Carole Douchy <carole.douchy@ehl.ch>
2019-02-26 17:37:47 +0100

    manip error

---
+++ app/helpers/recipes_helper.rb
@@ -0,0 +1,13 @@
+module RecipesHelper
+  def all_recipes_skipped?(array_of_ids)
+    Recipe.count == array_of_ids.count
+  end
+
+  def array_of_burned_ids(params_array)
+    recipe_ids = []
+    return recipe_ids << @recipe.id if params_array.nil? || all_recipes_skipped?(params_array)
+    recipe_ids << params_array
+    recipe_ids << @recipe.id
+    return recipe_ids.flatten
+  end
+end
