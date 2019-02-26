e7e50dfe5858d604f58b61ba593e1ebe2a5fe2d4
Carole Douchy <carole.douchy@ehl.ch>
2019-02-26 16:49:07 +0100

    display new recipe not already shown

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,14 @@
 class RecipesController < ApplicationController
   def index
-    @recipe = Recipe.all.sample
+    recipe_ids = params[:recipe_ids]
+    @recipe = select_unshown_recipes(recipe_ids).sample
+  end
+
+  private
+
+  def select_unshown_recipes(array_of_ids)
+    # methods returns an array of recipes not yiked by user yet
+    return Recipe.all if array_of_ids.blank?
+    Recipe.all.reject{ |recipe| array_of_ids.include?(recipe.id.to_s) }
   end
 end
