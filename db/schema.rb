2d175a8a253e47386888e0477beae16c6bbddf16
Carole Douchy <carole.douchy@ehl.ch>
2019-02-26 09:55:21 +0100

    schema à jour

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,42 @@
 
 ActiveRecord::Schema.define(version: 2019_02_25_164359) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "composants", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "recipe_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_composants_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_composants_on_recipe_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "price_100gr"
+    t.integer "kcal_100gr"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "orders", force: :cascade do |t|
+    t.bigint "users_id"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["users_id"], name: "index_orders_on_users_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.text "name"
+    t.string "category"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,5 +66,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_164359) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "composants", "ingredients"
+  add_foreign_key "composants", "recipes"
   add_foreign_key "orders", "users", column: "users_id"
 end
