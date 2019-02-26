55f0eba463704079a9a21885502236acc6ac215d
Carole Douchy <carole.douchy@ehl.ch>
2019-02-26 17:14:14 +0100

    refacto code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,6 @@ class RecipesController < ApplicationController
   def select_unshown_recipes(array_of_ids)
     # methods returns an array of recipes not yiked by user yet
     return Recipe.all if array_of_ids.blank? || all_recipes_skipped?(array_of_ids)
-    Recipe.all.reject{ |recipe| array_of_ids.include?(recipe.id.to_s) }
+    Recipe.all.reject { |recipe| array_of_ids.include?(recipe.id.to_s) }
   end
 end
