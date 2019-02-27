b6ea11a0689011dd09b9b2730724af2cbafc833c
Carole Douchy <carole.douchy@ehl.ch>
2019-02-27 11:24:36 +0100

    info button pimped and redirecting to recipes#show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,6 +6,12 @@ class RecipesController < ApplicationController
     @recipe = select_unshown_recipes(recipe_ids).sample
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+
   def select_unshown_recipes(array_of_ids)
     # methods returns an array of recipes not yiked by user yet
     return Recipe.all if array_of_ids.blank? || all_recipes_skipped?(array_of_ids)
