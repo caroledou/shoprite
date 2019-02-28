1ecfa1c3c0a480d2ffff4e121fcc3c32b20c1eb3
Carole Douchy <carole.douchy@ehl.ch>
2019-02-28 17:42:26 +0100

    click on i to flip card

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,11 @@
 //= require rails-ujs
 //= require_tree .
-// $('.flip-card .flip-card-inner').click(function() {
-//     $(this).closest('.flip-card').toggleClass('hover');
-//     $(this).css('transform, rotateY(180deg)');
-// });
 
 // Selectionner le noeud du DOM qui a classe .flip-card
 const flipcard = document.querySelector(".flip-card .flip-card-inner");
-flipcard.addEventListener("click", (event) => {
-  console.log(event);
-  console.log(event.currentTarget);
+const i = document.querySelector(".card-info");
+// Ecouter le click sur ce noeud
+i.addEventListener("click", (event) => {
+  // Dans le callback, toggler la classe hover sur .flip-card
   flipcard.classList.toggle("hover");
 });
-// Ecouter le click sur ce noeud
-// Dans le callback, toggler la classe hover sur .flip-card
