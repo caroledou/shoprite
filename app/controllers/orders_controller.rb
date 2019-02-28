b16566203641dcd88eb38ec22cda7c0a9ff543ac
Carole Douchy <carole.douchy@ehl.ch>
2019-02-28 14:15:33 +0100

    added @order_details in OrderController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,9 @@ class OrdersController < ApplicationController
     @order = Order.new(user_id: current_user.id, status: 'pending')
     @order.save!
     # Need to create the order_details for each composant
-    # @order_detail = OrderDetail.new(order: @order)
+    @composant = Composant.find_by(recipe_id: @recipe.id)
+    @order_detail = OrderDetail.new(order: @order, composant: @composant)
+    @order_detail.save!
     redirect_to order_path(@order)
   end
 end
