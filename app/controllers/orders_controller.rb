80dc64ca4ded1b06f4754b5b52378aa33e593160
Carole Douchy <carole.douchy@ehl.ch>
2019-02-28 15:07:43 +0100

    Composants displayed correctly to order#show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,9 +8,11 @@ class OrdersController < ApplicationController
     @order = Order.new(user_id: current_user.id, status: 'pending')
     @order.save!
     # Need to create the order_details for each composant
-    @composant = Composant.find_by(recipe_id: @recipe.id)
-    @order_detail = OrderDetail.new(order: @order, composant: @composant)
-    @order_detail.save!
+    composants = Composant.where(recipe_id: @recipe.id)
+    composants.each do |composant|
+      order_detail = OrderDetail.new(order: @order, composant: composant)
+      order_detail.save!
+    end
     redirect_to order_path(@order)
   end
 end
