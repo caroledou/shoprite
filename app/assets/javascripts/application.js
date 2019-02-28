20295158c1d5d922d043124abaf6a0b58eb17c4b
Carole Douchy <carole.douchy@ehl.ch>
2019-02-28 17:32:25 +0100

    flipcard on click working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,16 @@
 //= require rails-ujs
 //= require_tree .
-$(document).on("click", ".flip-card", function () {
-    $(this).toggleClass('hover');
+// $('.flip-card .flip-card-inner').click(function() {
+//     $(this).closest('.flip-card').toggleClass('hover');
+//     $(this).css('transform, rotateY(180deg)');
+// });
+
+// Selectionner le noeud du DOM qui a classe .flip-card
+const flipcard = document.querySelector(".flip-card .flip-card-inner");
+flipcard.addEventListener("click", (event) => {
+  console.log(event);
+  console.log(event.currentTarget);
+  flipcard.classList.toggle("hover");
 });
+// Ecouter le click sur ce noeud
+// Dans le callback, toggler la classe hover sur .flip-card
