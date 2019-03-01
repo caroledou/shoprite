11e116b9dbb63b243422b269648bec5af91a760b
Carole Douchy <carole.douchy@ehl.ch>
2019-03-01 14:55:03 +0100

    changed models

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,4 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :composant
-  has_many :recipes, through: :composants
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,5 @@
 class Recipe < ApplicationRecord
   has_many :composants, dependent: :destroy
-  has_many :order_details, through: :composants
   mount_uploader :photo, PhotoUploader
   RECIPE_CATEGORIES = ["Pasta", "Curry", "Salad"]
   validates :category, inclusion: { in: RECIPE_CATEGORIES }
