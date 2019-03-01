26cba6f49d6b10da32ed462ea5f6f70a807aab23
Carole Douchy <carole.douchy@ehl.ch>
2019-03-01 09:05:12 +0100

    changed js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,4 @@
 //= require rails-ujs
 //= require_tree .
 
-// Selectionner le noeud du DOM qui a classe .flip-card
-const flipcard = document.querySelector(".flip-card .flip-card-inner");
-const i = document.querySelector(".card-info");
-// Ecouter le click sur ce noeud
-i.addEventListener("click", (event) => {
-  // Dans le callback, toggler la classe hover sur .flip-card
-  flipcard.classList.toggle("hover");
-});
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+
+// Selectionner le noeud du DOM qui a classe .flip-card
+const flipcard = document.querySelector(".flip-card .flip-card-inner");
+const i = document.querySelector(".card-info");
+// Ecouter le click sur ce noeud
+i.addEventListener("click", (event) => {
+  // Dans le callback, toggler la classe hover sur .flip-card
+  flipcard.classList.toggle("hover");
+});
