ca7cd3405065ccd20e7654ae87d9e2d9ef0713b4
Albert Campillo <albertcampillo@gmail.com>
2019-03-04 17:16:56 +0100

    seed new dabatase updated

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :composants, dependent: :destroy
   mount_uploader :photo, PhotoUploader
-  RECIPE_CATEGORIES = ["Pasta", "Curry", "Salad"]
+  RECIPE_CATEGORIES = ['Beef', 'Chicken', 'Desert', 'Lamb', 'Miscellaneous', 'Pasta', 'Pork', 'Seafood', 'Side', 'Starter', 'Vegan', 'Vegetarian']
   validates :category, inclusion: { in: RECIPE_CATEGORIES }
 end
