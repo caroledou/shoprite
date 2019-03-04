61857899c29a822914206068b87e4ff5764d595c
Carole Douchy <carole.douchy@ehl.ch>
2019-03-04 09:57:08 +0100

    working on create cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,27 +4,48 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.find(params[:recipe_id])
     # Find exists already a pending order
-    if order_pending = Order.find_by(status: 'pending')
-    # Use the pending order
-      @order = order_pending
+    if @order = Order.find_by(user_id: current_user.id, status: 'pending')
+      array_of_recipes = @order.recipes.values
+      unless array_of_recipes.include?(@recipe.id)
+      # Need to create the order_details for each composant
+        composants = Composant.where(recipe_id: @recipe.id)
+        composants.each do |composant|
+          order_detail = OrderDetail.new(order: @order, composant: composant)
+          order_detail.save!
+        end
+      end
+      [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday].each do |day|
+        @order.recipes[day] = @recipe.id if @order.recipes[day] == ""
+        break if @order.recipes[day] == @recipe.id
+      end
+      @order.save!
     else
-      @order = Order.new(user_id: current_user.id, status: 'pending')
+      recipes = {
+        monday: "",
+        tuesday: "",
+        wednesday: "",
+        thursday: "",
+        friday: "",
+        saturday: "",
+        sunday: ""
+      }
+      @order = Order.new(user_id: current_user.id, status: 'pending', recipes: recipes)
+      array_of_recipes = @order.recipes.values
+      unless array_of_recipes.include?(@recipe.id)
+      # Need to create the order_details for each composant
+        composants = Composant.where(recipe_id: @recipe.id)
+        composants.each do |composant|
+          order_detail = OrderDetail.new(order: @order, composant: composant)
+          order_detail.save!
+        end
+      end
+      recipes[:monday] = @recipe.id
+      @order.recipes = recipes
       @order.save!
     end
-    # array vide
-    @recipes = []
     # ajoute recette de params si pas deja array
-    @recipe = Recipe.find(params[:recipe_id])
-    unless @recipes.include?(@recipe)
-      @recipes.push(@recipe)
-    # Need to create the order_details for each composant
-      composants = Composant.where(recipe_id: @recipe.id)
-      composants.each do |composant|
-        order_detail = OrderDetail.new(order: @order, composant: composant)
-        order_detail.save!
-      end
-    end
-    redirect_to order_path(@order, @recipes)
+    redirect_to order_path(@order)
   end
 end
