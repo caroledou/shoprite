e5fed3e29098757f3cba21944f5351b8578374cd
Carole Douchy <carole.douchy@ehl.ch>
2019-03-04 19:43:59 +0100

    flip back working

--- app/javascript/packs/flipback.js
+++ app/javascript/packs/flipback.js
@@ -1,8 +1,7 @@
 const flipcard = document.querySelector(".flip-card .flip-card-inner");
-const i = document.querySelector(".card-info");
+const i = document.querySelector("#show-back");
 // Ecouter le click sur ce noeud
 i.addEventListener("click", (event) => {
   // Dans le callback, toggler la classe hover sur .flip-card
   flipcard.classList.toggle("hover");
 });
-
