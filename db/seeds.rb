ad2d351ef73e9a9d9422e82c7faf76a7e90b65e4
Albert Campillo <albertcampillo@gmail.com>
2019-03-05 16:07:28 +0100

    changes on seed - composants random price in cents

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,7 +68,7 @@ recipe_id_db.each do |id|
         recipe_id: new_recipe.id,
         ingredient: recipe["meals"].first["strIngredient#{number}"],
         quantity: recipe["meals"].first["strMeasure#{number}"],
-        price_cents: rand(1..20)*100.to_i
+        price_cents: (rand(1..2000)/100).round.to_i
       )
       new_composant.save!
       composant_counter += 1
