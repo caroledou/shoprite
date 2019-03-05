3a92cd2c7a0417e9e89e7d8a27a158c3ee72a0bb
Carole Douchy <carole.douchy@ehl.ch>
2019-03-05 17:08:43 +0100

    organizing JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "bootstrap";
 
 import "../plugins/flatpickr"
-
-
+import "../plugins/flipback"
+import "../plugins/flipfront"
+import "../plugins/recipe_dropdown"
---
+++ app/javascript/plugins/flipfront.js
@@ -0,0 +1,9 @@
+const flipcard = document.querySelector(".flip-card .flip-card-inner");
+const i = document.querySelector(".card-info");
+// Ecouter le click sur ce noeud
+if (i) {
+  i.addEventListener("click", (event) => {
+    // Dans le callback, toggler la classe hover sur .flip-card
+    flipcard.classList.toggle("hover");
+  });
+};
