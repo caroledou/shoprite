bb203868af3d821eec590eadeab3a4ce2d1adc82
Carole Douchy <carole.douchy@ehl.ch>
2019-03-05 15:54:12 +0100

    method create delivery working

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,10 +1,26 @@
 class DeliveriesController < ApplicationController
+  def show
+    @delivery = Delivery.find(params[:id])
+  end
+
   def new
+    @order = Order.find(params[:order_id])
+    @delivery = Delivery.new
   end
 
   def create
+    @order = Order.find(params[:order_id])
+    @delivery = Delivery.new(delivery_params)
+    @delivery.user = current_user
+    @delivery.order = @order
+    if @delivery.save
+      redirect_to delivery_path(@delivery.id)
+    else
+      render :new
+    end
   end
 
-  def show
+  def delivery_params
+    params.require(:delivery).permit(:order_id, :user_id, :status, :date, :time)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :recipes, only: [:index, :show]
-  resources :orders, only: [:create, :show]
-  resources :deliveries, only: [:new, :create, :show]
+  resources :orders, only: [:create, :show] do
+    resources :deliveries, only: [:new, :create]
+  end
+  resources :deliveries, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
